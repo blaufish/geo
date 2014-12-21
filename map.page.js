6dc6cdd37ac34e781e00f926dc88d45aaedbce2d
blaufish <peter.sj.magnusson+github@gmail.com>
2014-12-21 02:38:15 +0100

    improved some

--- map.page.js
+++ map.page.js
@@ -1,32 +1,42 @@
-/*
- * Google Maps documentation: http://code.google.com/apis/maps/documentation/javascript/basics.html
- * Geolocation documentation: http://dev.w3.org/geo/api/spec-source.html
- */
+
 $( document ).on( "pageinit", "#map-page", function() {
-    if ( navigator.geolocation ) {
-        function success(pos) {
-            // Location found, show map with these coordinates
-            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-        }
-        function fail(error) {
-            $("#console").show();
-            $("#console").text("Error: " + error.message);
-        }
-        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
-        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+    function success(pos) {
+        drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        consoleLog("Current: " + pos.coords.latitude + " " + pos.coords.longitude + " (" + pos.coords.accuracy + "m)");
     }
-    function drawMap(latlng) {
+    function fail(error) {
+        consoleLog("Error: " + error.message)
+    }
+    navigator.geolocation.watchPosition(success, fail, {maximumAge: 0, enableHighAccuracy:true, timeout: 500});
+});
+
+function consoleLog(message) {
+    $("#console").show();
+    $("#console").text(message);
+}
+
+var map = null;
+var currentMarker = null;
+function initMap(latlng) {
+    if (map == null) {
         var myOptions = {
-            zoom: 30,
+            zoom: 20,
             center: latlng,
             mapTypeId: google.maps.MapTypeId.SATELLITE
         };
-        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-        // Add an overlay to the map of current lat/lng
-        var marker = new google.maps.Marker({
+        map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+    };
+    if (currentMarker == null) {
+        currentMarker = new google.maps.Marker({
             position: latlng,
             map: map,
-            title: "Greetings!"
-        });
-    }
-});
+            title: "Current Location!"
+        });    
+    }   
+}
+function drawMap(latlng) {
+    initMap(latlng);
+    map.panTo(latlng);
+    currentMarker.setPosition(latlng);
+
+}
