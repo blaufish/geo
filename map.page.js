c339fe16ef1b5b7affb27808c3a7bee4be587892
blaufish <peter.sj.magnusson+github@gmail.com>
2014-12-21 03:29:58 +0100

    got averaging working, perhaps

--- map.page.js
+++ map.page.js
@@ -1,18 +1,57 @@
 
 $( document ).on( "pageinit", "#map-page", function() {
     function success(pos) {
-        drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-        consoleLog("Current: " + pos.coords.latitude + " " + pos.coords.longitude + " (" + pos.coords.accuracy + "m)");
+        update(pos);
     }
     function fail(error) {
         consoleLog("Error: " + error.message)
     }
     navigator.geolocation.watchPosition(success, fail, {maximumAge: 0, enableHighAccuracy:true, timeout: 500});
+    setInterval(function() { drawMap(); }, 10000);
+    setInterval(function() { average(); }, 1000);
 });
 
 function consoleLog(message) {
     $("#console").show();
     $("#console").text(message);
+}
+
+var currentPosition;
+function update(pos) {
+    currentPosition = {
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude
+    };
+    initMap();
+    //consoleLog("Current: " + pos.coords.latitude + " " + pos.coords.longitude + " (" + pos.coords.accuracy + "m)");
+}
+
+var doAverage = true;
+var averagePosition;
+var averageBasePosition;
+var averageOffset = {
+    latitude: 0,
+    longitude: 0,
+    count: 0
+};
+var averageOffsetCount = 0;
+function average() {
+    if (!doAverage) return;
+    var pos = currentPosition;
+    if (!averageBasePosition) {
+        averageBasePosition = pos;
+        return;
+    }
+    var latOffset = pos.latitude - averageBasePosition.latitude;
+    var lonOffset = pos.longitude - averageBasePosition.longitude;
+    averageOffset.latitude += latOffset;
+    averageOffset.longitude += lonOffset;
+    averageOffset.count++;
+    averagePosition = {
+        latitude: averageBasePosition.latitude + averageOffset.latitude/averageOffset.count,
+        longitude: averageBasePosition.longitude + averageOffset.longitude/averageOffset.count
+    }
+    consoleLog("Averaging: " + averagePosition.latitude + " " + averagePosition.longitude + " (" + (averageOffset.count+1) + " samples)");
 }
 
 var map = null;
@@ -34,7 +73,8 @@ function initMap(latlng) {
         });    
     }   
 }
-function drawMap(latlng) {
+function drawMap() {
+    var latlng =  new google.maps.LatLng(currentPosition.latitude, currentPosition.longitude);
     initMap(latlng);
     map.panTo(latlng);
     currentMarker.setPosition(latlng);
