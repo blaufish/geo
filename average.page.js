408b53cfdf5b1d337681cac69d946928b32c47ee
blaufish <peter.sj.magnusson+github@gmail.com>
2014-12-23 00:15:32 +0100

    fixed formatting

--- average.page.js
+++ average.page.js
@@ -3,5 +3,24 @@ $( document ).on( "pageinit", "#average-page", function() {
 });
 
 function updateAveragePage() {
-	$("#gps-current-stats").text(currentPosition.longitude + " " + currentPosition.latitude + " (" + currentPosition.accuracy+ " m)" );
+	if(currentPosition === undefined) return;
+	$("#gps-current-stats-accuracy").text(currentPosition.accuracy+ " m" );
+	$("#gps-current-stats-position").text(fmt(currentPosition.longitude,'N','S') + " " + fmt(currentPosition.latitude,'E','W') );
+}
+
+function fmt(number, ne, sw) {
+	var letter = (number >= 0) ? ne : sw;
+	var abs = Math.abs(number);
+	var deg = Math.trunc(number);
+	var decimals = abs - deg;
+	var decimals60 = decimals * 60;
+	var minutes = Math.trunc(decimals60);
+	var minutesDecimals = pad(Math.trunc((decimals60-minutes) * 10000), 4);
+	return letter + " " + deg + " " + minutes + "." + minutesDecimals;
+}
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
 }
